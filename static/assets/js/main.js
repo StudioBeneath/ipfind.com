26bddce8e43a5a86ec1cf5175807d0a1a15fea7e
Jin Su Park <jinsupark@icloud.com>
2018-11-19 00:54:07 +1300

    U

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -469,3 +469,49 @@ var dropdown = new Dropdown();
 var notice = new Notice();
 // Initialize Slidemenu
 var slidemenu = new Slidemenu();
+
+$( document ).ready(function() {
+  console.log("document is ready");
+
+  $('.js-trigger-ruby').on( "click", function() {
+    $('.js-home-language-switch').removeClass('is-active');
+    $(this).toggleClass('is-active');
+
+    $('.js-snippet').hide();
+    $('.js-snippet-ruby').fadeIn('fast');
+  });
+
+  $('.js-trigger-php').on( "click", function() {
+    $('.js-home-language-switch').removeClass('is-active');
+    $(this).toggleClass('is-active');
+
+    $('.js-snippet').hide();
+    $('.js-snippet-php').fadeIn('fast');
+  });
+
+  $('.js-trigger-js').on( "click", function() {
+    $('.js-home-language-switch').removeClass('is-active');
+    $(this).toggleClass('is-active');
+
+    $('.js-snippet').hide();
+    $('.js-snippet-js').fadeIn('fast');
+  });
+
+  $('.js-trigger-cs').on( "click", function() {
+    $('.js-home-language-switch').removeClass('is-active');
+    $(this).toggleClass('is-active');
+
+    $('.js-snippet').hide();
+    $('.js-snippet-cs').fadeIn('fast');
+  });
+
+  $('.js-trigger-python').on( "click", function() {
+    $('.js-home-language-switch').removeClass('is-active');
+    $(this).toggleClass('is-active');
+
+    $('.js-snippet').hide();
+    $('.js-snippet-python').fadeIn('fast');
+  });
+
+
+});
